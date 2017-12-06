91be868298a20d9681805725d0fbc5dcb8abc544
codincodee <codincodee@outlook.com>
2017-12-06 17:31:05 +0800

    Added FoV boundary line

--- ce30_pointcloud_viewer/point_cloud_viewer.cpp
+++ ce30_pointcloud_viewer/point_cloud_viewer.cpp
@@ -84,18 +84,124 @@ void PointCloudViewer::UpdatePointCloudDisplay(
   viz.UpdatePointCloud(cloud);
 }
 #else
+
+#include <random>
+
+std::random_device gRd;
+std::mt19937 gGen(gRd());
+
+inline static float to_rad(const float& deg) {
+  return deg * M_PI / 180.0f;
+}
+
 void PointCloudViewer::UpdatePointCloudDisplay(
     const Scan &scan, PointCloudViz &viz) {
   PointCloud cloud;
-  float delta_y = 30.0f / 320;
-  for (float y = -15.0f; y < 15.0f; y += delta_y) {
-    float x = 26.0f;
-    if (y <= 7.5f && y >= -7.5f) {
-      x = 28.0f;
+
+  float delta_y = 0.1;
+  // Bulk Alpha
+  {
+    uniform_real_distribution<> x_dis(-0.00, 0.00);
+//    uniform_real_distribution<> x_dis(-0.03, 0.03);
+    uniform_real_distribution<> y_dis(-0.00, 0.00);
+    uniform_real_distribution<> z_dis(-0.00, 0.00);
+
+    float y_left = -5.0f;
+    float y_right = -3.0f;
+    float x_left = abs(y_left) * tan(to_rad(60));
+    float x_right = x_left + 1.5f;
+    float tan_alpha = (x_right - x_left) / (y_right - y_left);
+    int i = 0;
+    for (float y = y_left; y <= y_right; y += delta_y) {
+      float x = x_left + delta_y * tan_alpha * (++i);
+      static float z_span = 0.2f;
+      static float z_down = -(z_span / 2);
+      static float z_delta = z_span / 20;
+      for (int z_index = 0; z_index < 20; ++z_index) {
+        float z = z_down + z_index * z_delta;
+        cloud.push_back(ce30_pcviz::Point(x + x_dis(gGen), -y + y_dis(gGen), z + z_dis(gGen)));
+      }
     }
-    float delta_z = 2.0f / 20;
-    for (float z = -1.0f; z < 1.0f; z+= delta_z) {
-      cloud.push_back(ce30_pcviz::Point(x, y, z));
+  }
+
+  // Bulk Bravo
+  {
+    uniform_real_distribution<> x_dis(-0.0, 0.00);
+//    uniform_real_distribution<> x_dis(-0.07, 0.07);
+    uniform_real_distribution<> y_dis(-0.00, 0.00);
+    uniform_real_distribution<> z_dis(-0.00, 0.00);
+
+    float y_left = -4.0f;
+    float y_right = 0.0f;
+    float x_left = 29.0f;
+    float x_right = 27.0f;
+    float tan_bulk = (x_right - x_left) / (y_right - y_left);
+    int i = 0;
+    for (float y = y_left; y <= y_right; y += delta_y) {
+      float x = x_left + delta_y * tan_bulk * (++i);
+      static float z_span = 0.8f;
+      static float z_down = -(z_span / 2);
+      static float z_delta = z_span / 20;
+      for (int z_index = 0; z_index < 20; ++z_index) {
+        float z = z_down + z_index * z_delta;
+        cloud.push_back(ce30_pcviz::Point(x + x_dis(gGen), -y + y_dis(gGen), z + z_dis(gGen)));
+      }
+    }
+  }
+
+  // Bulk Charlie
+  {
+    uniform_real_distribution<> x_dis(-0.00, 0.00);
+//    uniform_real_distribution<> x_dis(-0.07, 0.07);
+    uniform_real_distribution<> y_dis(-0.00, 0.00);
+    uniform_real_distribution<> z_dis(-0.00, 0.00);
+
+    float y_left = 0.0f;
+    float y_right = 2.0f;
+    float x_left = 27.0f;
+    float x_right = 29.0f;
+    float tan_bulk = (x_right - x_left) / (y_right - y_left);
+    int i = 0;
+    for (float y = y_left; y <= y_right; y += delta_y) {
+      float x = x_left + delta_y * tan_bulk * (++i);
+      static float z_span = 0.8f;
+      static float z_down = -(z_span / 2);
+      static float z_delta = z_span / 20;
+      for (int z_index = 0; z_index < 20; ++z_index) {
+        float z = z_down + z_index * z_delta;
+        cloud.push_back(ce30_pcviz::Point(x + x_dis(gGen), -y + y_dis(gGen), z + z_dis(gGen)));
+      }
+    }
+  }
+
+  // Bulk Delta
+  {
+    uniform_real_distribution<> x_dis(-0.00, 0.00);
+//    uniform_real_distribution<> x_dis(-0.05, 0.05);
+    uniform_real_distribution<> y_dis(-0.00, 0.00);
+    uniform_real_distribution<> z_dis(-0.00, 0.00);
+
+    float y_left = 2.0f;
+    float y_right = 8.0f;
+    float y_bisect = 5.0f;
+    float x_right = abs(y_right) * tan(to_rad(60));
+    float x_left = x_right;
+    float tan_alpha = (x_right - x_left) / (y_right - y_left);
+    int i = 0;
+    for (float y = y_left; y <= y_right; y += delta_y) {
+      float x = x_left + delta_y * tan_alpha * (++i);
+      static float z_span = 0.6f;
+      static float z_down = -(z_span / 2);
+      static float z_delta = z_span / 20;
+      for (int z_index = 0; z_index < 20; ++z_index) {
+        float z = z_down + z_index * z_delta;
+        if (y >= y_left && y <= y_bisect) {
+          if (z_index >= 4) {
+            continue;
+          }
+        }
+        cloud.push_back(ce30_pcviz::Point(x + x_dis(gGen), -y + y_dis(gGen), z + z_dis(gGen)));
+      }
     }
   }
   viz.UpdatePointCloud(cloud);
