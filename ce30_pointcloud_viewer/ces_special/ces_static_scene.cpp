eddb0f36e8f51508ffe1b76204d4e2c3e6fce900
codincodee <codincodee@outlook.com>
2017-12-18 00:57:48 -0800

    ongoing

--- ce30_pointcloud_viewer/ces_special/ces_static_scene.cpp
+++ ce30_pointcloud_viewer/ces_special/ces_static_scene.cpp
@@ -2,9 +2,11 @@
 #include <iostream>
 
 using namespace std;
+using namespace pcl;
+using namespace pcl::visualization;
 
 CESStaticScene::CESStaticScene()
-  : Scene(nullptr), showing_(false)
+  : Scene(nullptr), showing_(false), last_showing_(false)
 {
 
 }
@@ -18,13 +20,35 @@ bool CESStaticScene::Showing() {
 }
 
 void CESStaticScene::Update() {
-  static bool last_showing = showing_;
-  if (last_showing != showing_) {
-    if (!showing_) {
-      Viz().addText3D("PPP", pcl::PointXYZ(1.0f, 1.0f, 1.0f), 1.0, 1.0, 1.0, 1.0, "PPP");
+  if (last_showing_ != showing_) {
+    if (showing_) {
+      scene_ids_ = DrawScene(Viz());
     } else {
-      Viz().removeText3D("PPP");
+      EraseScene(Viz(), scene_ids_);
     }
   }
-  last_showing = showing_;
+  last_showing_ = showing_;
+}
+
+vector<string> CESStaticScene::DrawScene(PCLVisualizer& viz) {
+  int id = -1;
+  viz.addLine(
+      PointXYZ(0.0f, -1.5f, -1.0f),
+      PointXYZ(0.0f, 1.5f, -1.0f), GetLineID(++id));
+  vector<string> ids;
+  ids.reserve(id);
+  for (int i = 0; i <= id; ++i) {
+    ids.push_back(GetLineID(i));
+  }
+  return ids;
+}
+
+void CESStaticScene::EraseScene(PCLVisualizer& viz, const vector<string> ids) {
+  for (auto& id : ids) {
+    viz.removeCorrespondences(id);
+  }
+}
+
+string CESStaticScene::GetLineID(const int &id) {
+  return "ces_scene_line_" + to_string(id);
 }
--- ce30_pointcloud_viewer/ces_special/ces_static_scene.h
+++ ce30_pointcloud_viewer/ces_special/ces_static_scene.h
@@ -11,7 +11,15 @@ public:
   bool Showing();
   void Update() override;
 private:
+  static std::vector<std::string> DrawScene(
+      pcl::visualization::PCLVisualizer& viz);
+  static void EraseScene(
+      pcl::visualization::PCLVisualizer& viz,
+      const std::vector<std::string> ids);
+  static std::string GetLineID(const int& id);
   bool showing_;
+  bool last_showing_;
+  std::vector<std::string> scene_ids_;
 };
 
 #endif // CES_STATIC_SCENE_H
