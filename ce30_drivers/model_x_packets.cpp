4531519a037b9603b13ed13f0d34d4265996ffa9
codincodee <33828315+codincodee@users.noreply.github.com>
2018-07-05 08:54:22 +0800

    Added parse

--- ce30_drivers/model_x_packets.cpp
+++ ce30_drivers/model_x_packets.cpp
@@ -1,4 +1,7 @@
 #include "model_x_packets.h"
+#ifndef M_PI
+#define M_PI 3.1415926
+#endif
 
 namespace ce30_drivers {
 namespace model_x {
@@ -7,13 +10,78 @@ Packet::Packet() {
   data.resize(TotalLength(), 0);
 }
 
-std::unique_ptr<ParsedPacket> Packet::Parse() {
-  return nullptr;
+Channel Packet::ParseChannel(
+    const uint32_t &A, const uint32_t &B, const uint16_t &t) {
+  Channel channel;
+  channel.x = 140.0 * std::sin(2 * M_PI * 0.005 * t);
+  channel.y =
+      (t < 500) ?
+          ((2.0 * 140.0 / 500.0) * t + 140.0) :
+          ((2.0 * 140.0 / (5000.0 - 500.0)) * t - 170);
+  channel.distance = 1.0 * A / B * 1000.0 * 3.0 * 1e8 / 2.0;
+  return channel;
 }
 
-// 1192
+std::unique_ptr<ParsedPacket> Packet::Parse() {
+  if (data.size() != TotalLength()) {
+    return nullptr;
+  }
+  if (!DetectHead(data[0], data[1])) {
+    return nullptr;
+  }
+  uint16_t t;
+  memcpy(&t, &data[2], 2);
+  int index = 4;
+  const int kSizeOfDim = SizeOfDim();
+  const int kSizeOfPacket = data.size();
+  std::unique_ptr<ParsedPacket> parsed(new ParsedPacket);
+  parsed->channels.reserve(NumChannel());
+  uint32_t A, B;
+  while (index < kSizeOfPacket) {
+    memcpy(&A, &data[index], kSizeOfDim);
+    index += kSizeOfDim;
+    memcpy(&B, &data[index], kSizeOfDim);
+    index += kSizeOfDim;
+    parsed->channels.push_back(ParseChannel(A, B, t));
+    ++t;
+  }
+  parsed->t = t;
+  return parsed;
+}
+
+// 1024
 int Packet::TotalLength() {
-  return 1192;
+  return 1204;
+}
+
+int Packet::SizeOfHead() {
+  return 2;
+}
+
+int Packet::SizeOfT() {
+  return 2;
+}
+
+int Packet::SizeOfDim() {
+  return 4;
+}
+
+int Packet::SizeOfChannel() {
+  return 2 * SizeOfDim();
+}
+
+int Packet::NumChannel() {
+  return (TotalLength() - SizeOfHead() - SizeOfT()) / SizeOfChannel();
+}
+
+bool Packet::DetectHead(const unsigned char &alpha, const unsigned char &beta) {
+  if (alpha != 0xFD) {
+    return false;
+  }
+  if (beta != 0xFE) {
+    return false;
+  }
+  return true;
 }
 
 // Scan
--- ce30_drivers/model_x_packets.h
+++ ce30_drivers/model_x_packets.h
@@ -8,16 +8,26 @@ namespace model_x {
 struct CE30_DRIVERS_API Channel : public ce30_drivers::Channel {
   static float DistanceMax();
   static float DistanceMin();
+  int x;
+  int y;
 };
 
 struct CE30_DRIVERS_API ParsedPacket : public ce30_drivers::ParsedPacket {
-
+  uint16_t t;
+  std::vector<Channel> channels;
 };
 
 struct CE30_DRIVERS_API Packet : public ce30_drivers::Packet {
   Packet();
   std::unique_ptr<ParsedPacket> Parse();
   inline static int TotalLength();
+  inline static int SizeOfHead();
+  inline static int SizeOfT();
+  inline static int SizeOfDim();
+  inline static int SizeOfChannel();
+  inline static int NumChannel();
+  static bool DetectHead(const unsigned char& alpha, const unsigned char& beta);
+  static Channel ParseChannel(const uint32_t& A, const uint32_t& B, const uint16_t& t);
 };
 
 class CE30_DRIVERS_API Scan : public ce30_drivers::Scan {
