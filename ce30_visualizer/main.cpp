143777e7add83d4f2ca089520e853da6237e6272
codincodee <codincodee@outlook.com>
2018-07-18 18:59:26 -0700

    Added version manifest to visualizer

--- ce30_visualizer/main.cpp
+++ ce30_visualizer/main.cpp
@@ -7,8 +7,12 @@
 #include "fake_point_cloud_viewer.h"
 #endif
 
+const std::string kCE30VisualizerVersion = "v1.0.0";
+
 int main(int argc, char *argv[])
 {
+  std::cout << "Software Version: " << kCE30VisualizerVersion << std::endl;
+
   QApplication app(argc, argv);
 #ifdef FAKE_POINTCLOUD
   FakePointCloudViewer viewer;
